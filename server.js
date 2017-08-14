b39195551df4554fe08fe21d03c1ae2036eab1cd
Curtis Timson <curt@live.co.uk>
2017-08-14 21:05:31 +0100

    Set logic for /latest-tweet

--- server.js
+++ server.js
@@ -3,9 +3,36 @@ var Twitter = require('twitter');
 
 var app = express();
 
-app.get('/', (req, res) => {
-   res.setHeader('Content-Type', 'application/json');
-   res.send(JSON.stringify({ a: 1 }));
+app.get('/latest-tweet', (req, res) => {
+
+    var client = new Twitter({
+      consumer_key: 'XXX',
+      consumer_secret: 'XXX',
+      access_token_key: 'XXX-XXX',
+      access_token_secret: 'XXX'
+    });
+
+    res.setHeader('Content-Type', 'application/json');
+
+    // https://dev.twitter.com/rest/reference/get/statuses/user_timeline
+    client.get('statuses/user_timeline', { screen_name: 'lvel', count: 1 }, function(error, tweets, response) {
+      if (!error) {
+
+        var latestTweet = {
+          text: tweets[0].text
+        };
+
+
+        res.send(JSON.stringify({ tweet: latestTweet }));
+      }
+      else {
+        res.status(500).json({ error: error });
+      }
+    });
+
+
+
+   //res.send(JSON.stringify({ a: 1 }));
 });
 
-app.listen(3000);
+app.listen(80);
