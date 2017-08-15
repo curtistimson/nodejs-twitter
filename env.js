e80696c09cfd52014c23b8e2c4f477e7996acfb8
Curtis Timson <curtis.timson@whitbread.com>
2017-08-15 17:26:12 +0100

    :sparkles: Add conditionally loaded env.js which can contain local configs

---
+++ env.js
@@ -0,0 +1,5 @@
+process.env.twitter_consumer_key = "";
+process.env.twitter_consumer_secret = ""
+process.env.twitter_access_token_key = "";
+process.env.twitter_access_token_secret = "";
+process.env.twitter_username = "";
